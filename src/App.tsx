5ba7c5f1079a84e3e2103387a8c9e730c331e168
Victor Pavageau <victor.pavageau@viacesi.fr>
2023-06-26 21:38:52 +0200

    Add authentication system

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import RouteHandler from "./routing";
+import { ConfigProvider } from "antd";
 
 function App() {
-  return <RouteHandler />;
+  const queryClient = new QueryClient();
+
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#000000",
+        },
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouteHandler />
+      </QueryClientProvider>
+    </ConfigProvider>
+  );
 }
 
 export default App;
